2c85390f475e8bea156fdd2b70a5557fd2b4ace5
hrishabh1008 <veterantyro@gmail.com>
2025-05-14 09:10:08 +0530

    videoPlayer card working properly

--- frontend/src/components/watch/VideoPlayerCard.jsx
+++ frontend/src/components/watch/VideoPlayerCard.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
-const VideoPlayerCard = () => {
+const VideoPlayerCard = ({ videoUrl }) => {
+  console.log(videoUrl);
+  const videoId = new URL(videoUrl).searchParams.get("v");
+
   return (
-    <div>VideoPlayerCard</div>
-  )
-}
+    <div className="aspect-video w-full rounded overflow-hidden">
+      <iframe
+        width="100%"
+        height="100%"
+        src={`https://www.youtube.com/embed/${videoId}`}
+        title="YouTube video player"
+        allowFullScreen
+        className="w-full h-[480px] rounded-lg"></iframe>
+    </div>
+  );
+};
 
-export default VideoPlayerCard
+export default VideoPlayerCard;
