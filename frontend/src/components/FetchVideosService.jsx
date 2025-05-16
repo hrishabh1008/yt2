b774592fc527636808ac2a31463b8f278b1f24b0
hrishabh1008 <veterantyro@gmail.com>
2025-05-17 03:58:37 +0530

    unused code commented

--- frontend/src/components/FetchVideosService.jsx
+++ frontend/src/components/FetchVideosService.jsx
@@ -11,25 +11,25 @@ export const getAllVideos = async () => {
   }
 };
 
-// Fetch video by ID
-export const getVideoById = async (id) => {
-  try {
-    const res = await api.get(`/api/videos/${id}`);
-    return res.data;
-  } catch (error) {
-    throw error.response?.data || error;
-  }
-};
+// // Fetch video by ID
+// export const getVideoById = async (id) => {
+//   try {
+//     const res = await api.get(`/api/videos/${id}`);
+//     return res.data;
+//   } catch (error) {
+//     throw error.response?.data || error;
+//   }
+// };
 
-// Search with multiple filters (e.g. title, category, etc.)
-export const searchVideos = async (queryParams) => {
-  try {
-    const res = await api.get("/api/videos/search", { params: queryParams });
-    return res.data;
-  } catch (error) {
-    throw error.response?.data || error;
-  }
-};
+// // Search with multiple filters (e.g. title, category, etc.)
+// export const searchVideos = async (queryParams) => {
+//   try {
+//     const res = await api.get("/api/videos/search", { params: queryParams });
+//     return res.data;
+//   } catch (error) {
+//     throw error.response?.data || error;
+//   }
+// };
 
 // Delete a video by ID
 export const deleteVideo = async (id) => {
